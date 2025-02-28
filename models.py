66e519b30f38e7780ff86fa2af781be510b4f315
D491786F039ADD4A3EA47195E701FA0F <144236605+D491786F039ADD4A3EA47195E701FA0F@users.noreply.github.com>
2025-02-28 09:17:19 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,15 @@
+from app import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    jobs = db.relationship('JobPost', backref='author', lazy=True)
+
+class JobPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
