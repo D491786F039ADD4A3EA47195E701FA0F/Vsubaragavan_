b15fe11debb9c0cb4cc9eda649bca43def651621
D491786F039ADD4A3EA47195E701FA0F <144236605+D491786F039ADD4A3EA47195E701FA0F@users.noreply.github.com>
2025-02-28 09:16:05 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from forms import RegistrationForm, LoginForm, JobPostForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+from models import User, JobPost
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.password == form.password.data:
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/post_job", methods=['GET', 'POST'])
+@login_required
+def post_job():
+    form = JobPostForm()
+    if form.validate_on_submit():
+        job = JobPost(title=form.title.data, description=form.description.data, author=current_user)
+        db.session.add(job)
+        db.session.commit()
+        flash('Your job has been posted!', 'success')
+        return redirect(url_for('jobs'))
+    return render_template('post_job.html', title='Post Job', form=form)
+
+@app.route("/jobs")
+def jobs():
+    jobs = JobPost.query.all()
+    return render_template('jobs.html', jobs=jobs)
+
+if __name__ == '__main__':
+    app.run(debug=True)
